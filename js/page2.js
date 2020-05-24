69347f5b8d644237cd8682df66a01f46382367de
Unknown <laurelenn-sangare@laposte.net>
2020-05-24 21:29:10 +0100

    Add comment for page 2 + update picture

--- js/page2.js
+++ js/page2.js
@@ -9,6 +9,18 @@ var img = document.querySelector('img');
 	img.addEventListener('click', onClick, false);
 
 	function onClick() {
+
+		/* Adapter : 
+
+		1. Random calculer le partiel parmi 4 possibilités (Loi : ???)
+		2. En fonction de ça : Calculer de cb on doit tourner la roue 
+			+ Ajouter X degres aléatoires (mais doit toujours tomber sur le bon partiel)
+		3. Random sur la note en fonction du typeIMAC et du partiel (Choix entre 4 lois proportionnelles ? voir drive)
+		4. Afficher résultat (alert), on click -> Next
+
+		*/
+		
+
 	    this.removeAttribute('style');
 	    
 	    var deg = mathFunction();
