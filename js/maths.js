d807f954068f0fe4aa32e101e9d6bbf9cb4269b5
Margaux <margauxvllt@gmail.com>
2020-05-25 18:36:34 +0200

    Explications

--- js/maths.js
+++ js/maths.js
@@ -1,4 +1,8 @@
 
+/*Une fonction gaussienne est sous le forme 
+f(x)= 1/ sigma*racine(2pi)  * e^{- (x-mu)^2  / 2\sigma^2
+On a donc pris sigma = 2 et mu = center 
+afin que notre courbe gaussienne soit un peu étalée et centrée en la valeur souhaitée*/
 function gauss(center, x){ 
 	return (20*Math.sqrt(2*Math.PI) *Math.exp(- Math.pow(x-center, 2)/8))/(2*Math.sqrt(2*Math.PI));
 }
